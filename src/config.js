da3be0e0300107d57092c4ba19af015efa672ba3
Jean Carlos Gonzalez Goyeneche <114271823+JeanGonzalez10@users.noreply.github.com>
2023-11-02 19:54:51 +0000

    change in config.js

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@ import { config } from "dotenv";
 
 config();
 
-const PORT = process.env.PORT || 3000;
-const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_PORT = process.env.DB_PORT || 3306;
-const DB_USER = process.env.DB_USER || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD || "$JeanCarlo$28";
-const DB_NAME = process.env.DB_NAME || "unipark";
+const PORT = process.env.PORT;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 
 export { PORT, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME };
